54e2e02aaf33fe37cf03022230f1a2deb8877eff
Sita Prajapati <sita.prajapati@yipl.com.np>
2014-11-19 11:13:58 +0545

    initial push

---
+++ districtTestScore/districtTestScore/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for districtTestScore project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'districtTestScore'
+
+SPIDER_MODULES = ['districtTestScore.spiders']
+NEWSPIDER_MODULE = 'districtTestScore.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'districtTestScore (+http://www.yourdomain.com)'
